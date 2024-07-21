9069ccb74061d8d74efeac627e907d0a6a2727aa
Mário Mello <marioesmello@gmail.coim>
2024-07-21 11:02:13 -0300

    chore: 🛠️ configuração inicial do repositório e remoção de arquivos desnecessários

--- docker/mysql/docker-entrypoint-initdb.d/setup.sql
+++ docker/mysql/docker-entrypoint-initdb.d/setup.sql
@@ -1,19 +1,9 @@
 CREATE
-DATABASE IF NOT EXISTS `identidade` COLLATE 'utf8_general_ci';
+DATABASE IF NOT EXISTS `default` COLLATE 'utf8_general_ci';
 GRANT ALL
 ON `identidade`.* TO 'default'@'%';
 
 CREATE
-DATABASE IF NOT EXISTS `test_identidade` COLLATE 'utf8_general_ci';
+DATABASE IF NOT EXISTS `test_default` COLLATE 'utf8_general_ci';
 GRANT ALL
 ON `test_identidade`.* TO 'default'@'%';
-
-CREATE
-DATABASE IF NOT EXISTS `catalogo` COLLATE 'utf8_general_ci';
-GRANT ALL
-ON `catalogo`.* TO 'default'@'%';
-
-CREATE
-DATABASE IF NOT EXISTS `test_catalogo` COLLATE 'utf8_general_ci';
-GRANT ALL
-ON `test_catalogo`.* TO 'default'@'%';
